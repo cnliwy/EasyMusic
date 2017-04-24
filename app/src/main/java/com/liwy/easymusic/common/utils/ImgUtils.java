a15a00d22fe77fa13447fbc11075374cd88f6c4c
cnliwy <cnliwy@163.com>
2017-04-24 11:25:19 +0800

    update

--- app/src/main/java/com/liwy/easymusic/common/utils/ImgUtils.java
+++ app/src/main/java/com/liwy/easymusic/common/utils/ImgUtils.java
@@ -1,12 +1,18 @@
 package com.liwy.easymusic.common.utils;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.liwy.easymusic.R;
+
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
 /**
  * Glide图片加载框架的使用封装类  
+ * 基础的图片操作封装类
  * Created by forezp on 16/8/10.
  */
 public class ImgUtils {
@@ -22,6 +28,23 @@ public class ImgUtils {
 
     public void display(Context context, String url, ImageView imageView){
         Glide.with(context).load(url).into(imageView);
+    }
+
+
+    /**
+     * 根据资源id获取drawable
+     * @param mContext
+     * @param rid
+     * @return
+     */
+    public static Drawable getDrawable(Context mContext,int rid){
+        Drawable drawable = null;
+        if (Build.VERSION.SDK_INT >= LOLLIPOP) {
+            drawable = mContext.getResources().getDrawable(rid, null);
+        }else{
+            drawable = mContext.getResources().getDrawable(rid);
+        }
+        return drawable;
     }
 
 }
